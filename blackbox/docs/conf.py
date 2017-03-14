967b4af5692a359e7ae0136a4b9c3e3818bc3e5d
meriamk <meriam.kharbat@gmail.com>
2017-03-14 15:13:33 +0000

    update source suffix for read the docs

--- blackbox/docs/conf.py
+++ blackbox/docs/conf.py
@@ -6,3 +6,4 @@ extensions = ['crate.sphinx.csv']
 # crate.theme sets html_favicon to favicon.png which causes a warning because it should be a .ico
 # and in addition there is no favicon.png in this project so it can't find the file
 html_favicon = None
+source_suffix = '.txt'
